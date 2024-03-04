a8e14249c0bd03a6bb773ae485bac8068d6d90f5
Ragul Prabakaran <113840624+ragulprabakaran1@users.noreply.github.com>
2024-03-04 16:38:48 +0530

    for loop ex 2.c
    Factorial using for loop

---
+++ for loop ex 2.c
@@ -0,0 +1,19 @@
+//Factorial using for loop
+#include <stdio.h>
+
+int main() {
+    
+    int i,n,fact=1;
+    
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    for(i=1; i<=n; i++)
+    {
+        fact=fact*i;
+    }
+   
+    printf("Fact = %d", fact);
+
+    return 0;
+}
